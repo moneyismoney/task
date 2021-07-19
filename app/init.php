8befb3b62e42ddf549b6b6be8f0e36b79f72a40c
Oleksandr Tymchenko <Oleksandr.Tymchenko@dreamscapenetworks.com>
2021-07-20 00:08:34 +0300

    initial MVC structure

---
+++ app/init.php
@@ -0,0 +1,9 @@
+<?php
+// @todo comment this when move to prod
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+require_once 'core/App.php';
+require_once 'core/Controller.php';
